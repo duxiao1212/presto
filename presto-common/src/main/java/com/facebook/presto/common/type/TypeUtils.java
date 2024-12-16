4cfe14a5457998b3e4d861a640ed887c24b788bf
Xiao Du <duxiao@meta.com>
2024-12-16 12:15:03 -0800

    Update the error type for ARRAY_NULL_ELEMENT_MSG to INVALID_FUNCTION_ARGUMENT in order to suppress the error for try(map_keys_by_top_n_values ...) when the value is an array with null.

--- presto-common/src/main/java/com/facebook/presto/common/type/TypeUtils.java
+++ presto-common/src/main/java/com/facebook/presto/common/type/TypeUtils.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.common.type;
 
+import com.facebook.presto.common.InvalidFunctionArgumentException;
 import com.facebook.presto.common.NotSupportedException;
 import com.facebook.presto.common.block.Block;
 import com.facebook.presto.common.block.BlockBuilder;
@@ -26,6 +27,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static com.facebook.presto.common.type.ArrayType.ARRAY_NULL_ELEMENT_MSG;
 import static com.facebook.presto.common.type.BigintType.BIGINT;
 import static com.facebook.presto.common.type.Decimals.encodeScaledValue;
 import static com.facebook.presto.common.type.DoubleType.DOUBLE;
@@ -188,9 +190,10 @@ public final class TypeUtils
 
     static void checkElementNotNull(boolean isNull, String errorMsg)
     {
-        if (isNull) {
-            throw new NotSupportedException(errorMsg);
+        if (!isNull) {
+            return;
         }
+        throw ARRAY_NULL_ELEMENT_MSG.equals(errorMsg) ? new InvalidFunctionArgumentException(errorMsg) :  new NotSupportedException(errorMsg);
     }
 
     static void validateEnumMap(Map<String, ?> enumMap)
